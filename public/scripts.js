661df8bc53a0c32d2c28a8567e68e5328815785c
Thiago Kachinsky <thiagokachidev@gmail.com>
2020-11-27 15:53:04 -0300

    Desafio Foodfy - front-end

--- public/scripts.js
+++ public/scripts.js
@@ -1,20 +1,22 @@
 const modalOverlay = document.querySelector(".modal-overlay")
-const modal = document.querySelector(".modal")
 const recipesDetail = document.querySelectorAll(".recipes-detail")
+const modalContent = document.querySelector(".modal-content")
+
 
 for(let recipe of recipesDetail) {
     recipe.addEventListener("click", function() {
-         const recipeImg = recipe.querySelector('img')
-         const recipeName = recipe.querySelector('h4')
-         const recipeAuthor = recipe.querySelector('p')
         modalOverlay.classList.add('active')
-        modalOverlay.querySelector("img").src = `${recipeImg}`
+        const image = recipe.querySelector('img').src
+        const nameRecipe = recipe.querySelector('h4').innerHTML
+        const author = recipe.querySelector('p').innerHTML
+        modalContent.querySelector('img').src = `${image}`
+        modalContent.querySelector('h4').innerText = `${nameRecipe}`
+        modalContent.querySelector('p').innerText = `${author}`
     })
 }
-
 
 // Lógica para fechar o modal
 document.querySelector('.close-modal').addEventListener("click", function() {
     modalOverlay.classList.remove('active')
-    modalOverlay.querySelector("iframe").src = ""
+    modalOverlay.querySelector("modal-content") = ""
 })
