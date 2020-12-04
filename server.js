45b7f4ae29d6a06c8b882d90515811f94ca9c3b7
Thiago Kachinsky <thiagokachidev@gmail.com>
2020-12-03 22:04:55 -0300

    Refatorando o Foodfy

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
+const receitas = require('./data')
+
+server.use(express.static('public'))
+
+server.set("view engine", 'njk')
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
 
 server.get("/", function(req, res) {
-    return res.send("Hi")
+    return res.render('index', { items: receitas })
+})
+
+server.get("/about", function(req, res) {
+    return res.render('about')
+})
+
+server.get("/recipes", function(req, res) {
+    return res.render('recipes', { items: receitas })
 })
 
 server.listen(5000, function() {
@@ -10,6 +31,7 @@ server.listen(5000, function() {
 })
 
 
-server.use(function(req, res) {
-    res.status(404).render("not-found");
-  });
+// Error 404
+// server.use(function(req, res) {
+//     res.status(404).render("not-found");
+//   });
